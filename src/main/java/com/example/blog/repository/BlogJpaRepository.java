3f787d5503113d303c5b912e9f3e7679ee4c121e
serinryu <serin0404@naver.com>
2023-07-14 16:41:21 +0900

    fix : @Modifying(clearAutomatically = true)

--- src/main/java/com/example/blog/repository/BlogJpaRepository.java
+++ src/main/java/com/example/blog/repository/BlogJpaRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.Query;
 public interface BlogJpaRepository extends JpaRepository<Blog, Long> {
 
     /* Blog Entity 에 BlogCount + 1 하는 메서드 만들고, 이를 사용하여 Dirty-Checking 으로 DB update
-    @Modifying
+    @Modifying(clearAutomatically = true)
     @Query("UPDATE Blog b SET b.blogCount = b.blogCount + 1 WHERE b.blogId = :blogId")
     void updateBlogCount(Long blogId);
      */
